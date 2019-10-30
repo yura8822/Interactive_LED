42a4269a80a0bbdb935d1abd464cdb43182fa742
yura8822 <yura8822m@gmail.com>
2019-10-30 15:04:20 +0300

    add DeviceListActivity.java

--- app/src/main/java/com/yura8822/bluetooth/BluetoothService.java
+++ app/src/main/java/com/yura8822/bluetooth/BluetoothService.java
@@ -32,9 +32,8 @@ public class BluetoothService {
 
     // Constants that indicate the current connection state
     public static final int STATE_NONE = 0;       // we're doing nothing
-    public static final int STATE_LISTEN = 1;     // now listening for incoming connections
-    public static final int STATE_CONNECTING = 2; // now initiating an outgoing connection
-    public static final int STATE_CONNECTED = 3;  // now connected to a remote device
+    public static final int STATE_CONNECTING = 1; // now initiating an outgoing connection
+    public static final int STATE_CONNECTED = 2;  // now connected to a remote device
 
     public BluetoothService(Context context, Handler handler) {
         mAdapter = BluetoothAdapter.getDefaultAdapter();
